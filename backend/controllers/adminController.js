f76580967dab2cc9c0a46ac6b1634085efdcdc92
Jitesh <jiteshgopale26@gmail.com>
2026-04-27 17:50:01 +0530

    create approve flat route

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -19,7 +19,35 @@ export const getPendingFlatsController = async (req, res) => {
     });
     return res
       .status(200)
-      .json({ message: "Sucessfully fetch all pending flats", flats });
+      .json({ message: "Sucessfully fetch all pending flats", data: flats });
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const approveFlatController = async (req, res) => {
+  try {
+    const flatId = Number(req.params.id);
+
+    const flat = await prismaClient.flat.findUnique({
+      where: {
+        id: flatId,
+      },
+    });
+
+    if (!flat) {
+      return res.status(404).json({
+        message: "flat not found",
+      });
+    }
+    const updateStatusFlat = await prismaClient.flat.update({
+      where: { id: flatId },
+      data: { status: "approved" },
+    });
+
+    return res
+      .status(200)
+      .json({ message: "Flat Approved Sucessfully", data: updateStatusFlat });
   } catch (error) {
     return res.status(500).json({ message: "Something went wrong" });
   }
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { roleMiddleware } from "../middlewares/roleMiddleware.js";
-import { getPendingFlatsController } from "../controllers/adminController.js";
+import { approveFlatController, getPendingFlatsController } from "../controllers/adminController.js";
 
 const adminRouter = express.Router();
 
-adminRouter.get('/flats/pending',authMiddleware,roleMiddleware("ADMIN"),getPendingFlatsController)
+adminRouter.get('/flats/pending',authMiddleware,roleMiddleware("ADMIN"),getPendingFlatsController);
+adminRouter.put('/flats/:id/approve',authMiddleware,roleMiddleware("ADMIN"),approveFlatController);
 
 export default adminRouter;
