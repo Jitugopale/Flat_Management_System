654de167fb87ecd34bd3fa983cc9a7c379725355
Jitesh <jiteshgopale26@gmail.com>
2026-04-28 13:32:13 +0530

    frontend done with testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,48 @@
+
 import './App.css'
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import UserDashboard from './pages/user/UserDashboard'
+import AdminDashboard from './pages/admin/AdminDashboard'
+import FlatApprovedPage from './pages/user/FlatApprovedPage'
+import MyFlatPage from './pages/user/MyFlatPage'
+import EnquiriesPage from './pages/user/EnquiriesPage'
+import EnquiriesForMyFlat from './pages/user/EnquiriesForMyFlat'
+import AdminDashPage from './pages/admin/AdminDashPage'
+import PendingFlats from './pages/admin/PendingFlats'
+import ApprovedFlats from './pages/admin/ApprovedFlats'
+import SoldFlats from './pages/admin/SoldFlats'
+import AllEnquiries from './pages/admin/AllEnquiries'
+import AddFlatPage from './pages/user/AddFlatPage'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
 
 function App() {
 
   return (
     <>
-    <h1 className='text-4xl text-red-500'>Frontend Flat Management</h1>
+     <Router>
+        <Routes>
+            <Route path='/' element={<Register/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/user-dash' element={<UserDashboard/>}>
+              <Route index element={<FlatApprovedPage/>}/>
+              <Route path='myflats' element={<MyFlatPage/>}/>
+              <Route path='myflats/add' element={<AddFlatPage/>}/>
+              <Route path='enquiries' element={<EnquiriesPage/>}/>
+              <Route path='enquiries/received' element={<EnquiriesForMyFlat/>}/>
+              
+            </Route>
+            <Route path='/admin-dash' element={<AdminDashboard/>}>
+              <Route index element={<AdminDashPage/>}/>
+              <Route path='pending' element={<PendingFlats/>}/>
+              <Route path='approved' element={<ApprovedFlats/>}/>
+              <Route path='sold' element={<SoldFlats/>}/>
+              <Route path='enquiriesAll' element={<AllEnquiries/>}/>
+              
+            </Route>
+        </Routes>
+     </Router>
     </>
   )
 }
